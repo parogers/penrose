5ddd8196a10e127778888b239ef2f41f5915831c
Peter Rogers <peter.rogers@gmail.com>
2021-07-08 22:28:27 -0400

    Add angular 12 starter project

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,28 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PenroseComponent } from './penrose/penrose.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider'; 
-import { MatCheckboxModule } from '@angular/material/checkbox'; 
-import { MatSelectModule } from '@angular/material/select'; 
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        PenroseComponent
-    ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        FormsModule,
-        MatSliderModule,
-        MatCheckboxModule,
-        MatSelectModule,
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
